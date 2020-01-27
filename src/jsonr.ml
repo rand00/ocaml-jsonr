34558eb7d47d657a0b3440385f5e9de3aa405e44
rand00 <oth.rand@gmail.com>
2020-01-27 20:22:06 +0100

    Corrected some 6-byte conversions + renaming + todo comments

--- src/jsonr.ml
+++ src/jsonr.ml
@@ -72,7 +72,7 @@ module Jsonr = struct
     end
     
     (*goto possibly check for integer overflow*)
-    let int_of_bytestring :
+    let int_of_byte_string :
       type int_local.
         (module IntAbstract with type t = int_local)
       -> of_int:(int -> int_local)
@@ -98,7 +98,7 @@ module Jsonr = struct
         ) (of_int 0, drop_bits_left)
         |> fun (result, _) -> result
     
-    let string_of_bytes bs = bs |> Array.map Char.chr |> CCString.of_array
+    let string_of_byte_array bs = bs |> Array.map Char.chr |> CCString.of_array
 
     let char_of_string s =
       assert (String.length s = 1);
@@ -107,15 +107,15 @@ module Jsonr = struct
     let int_of_byte_list ~drop_bits_left bytes =
       bytes
       |> CCString.of_list 
-      |> int_of_bytestring (module CCInt) ~of_int:CCFun.id ~drop_bits_left
+      |> int_of_byte_string (module CCInt) ~of_int:CCFun.id ~drop_bits_left
 
     let magic_number =
       [| 0b11010011; 0b01001010; 0b01010010; 0b01100010 |]
-      |> string_of_bytes
+      |> string_of_byte_array
 
     let version =
       [| 0b00000001 |] 
-      |> string_of_bytes
+      |> string_of_byte_array
 
     open CCOpt.Infix 
 
@@ -127,7 +127,6 @@ module Jsonr = struct
     and parse_object ~take ~length =
       let fields =
         List.init length (fun _ ->
-          (*> goto q joakim; is it really right to have any value as field-key?*)
           ( parse_value ~take >>= function
             | `String str -> parse_value ~take >|= fun json -> (str, json)
             | _ -> None )
@@ -141,7 +140,7 @@ module Jsonr = struct
       take length >|= fun str ->
       `String str
 
-    (*goto find out if this is the correct json implementation *)
+    (*goto make a 'data-url' in json string*)
     and parse_binary_data ~take ~length = parse_string ~take ~length
 
     and parse_value : take:(int -> string option) -> json option =
@@ -181,67 +180,67 @@ module Jsonr = struct
 
       (*Array*)
       | 1::1::1::1::0::0::0::0::_ ->
-        take 2 >|= int_of_bytestring (module CCInt) ~of_int:CCFun.id
+        take 2 >|= int_of_byte_string (module CCInt) ~of_int:CCFun.id
           ~drop_bits_left:0 >>= fun length ->
         parse_array ~take ~length
           
       (*Object*)
       | 1::1::1::1::0::0::0::1::_ ->
-        take 2 >|= int_of_bytestring (module CCInt) ~of_int:CCFun.id
+        take 2 >|= int_of_byte_string (module CCInt) ~of_int:CCFun.id
           ~drop_bits_left:0 >>= fun length ->
         parse_object ~take ~length
 
       (*String*)
       | 1::1::1::1::0::0::1::0::_ ->
-        take 2 >|= int_of_bytestring (module CCInt) ~of_int:CCFun.id
+        take 2 >|= int_of_byte_string (module CCInt) ~of_int:CCFun.id
           ~drop_bits_left:0 >>= fun length ->
         parse_string ~take ~length
 
       (*Binary data*)
       | 1::1::1::1::0::0::1::1::_ ->
-        take 2 >|= int_of_bytestring (module CCInt) ~of_int:CCFun.id
+        take 2 >|= int_of_byte_string (module CCInt) ~of_int:CCFun.id
           ~drop_bits_left:0 >>= fun length ->
         parse_binary_data ~take ~length
 
       (*Array*)
       | 1::1::1::1::0::1::0::0::_ ->
-        take 6 >|= int_of_bytestring (module CCInt) ~of_int:CCFun.id
-          ~drop_bits_left:0 >>= fun length ->
+        take 6 >|= int_of_byte_string (module CCInt64) ~of_int:CCInt64.of_int
+          ~drop_bits_left:0 >|= CCInt64.to_int >>= fun length -> (*lossy*)
         parse_array ~take ~length
 
       (*Object*)
       | 1::1::1::1::0::1::0::1::_ ->
-        take 6 >|= int_of_bytestring (module CCInt) ~of_int:CCFun.id
-          ~drop_bits_left:0 >>= fun length ->
+        take 6 >|= int_of_byte_string (module CCInt64) ~of_int:CCInt64.of_int
+          ~drop_bits_left:0 >|= CCInt64.to_int >>= fun length -> (*lossy*)
         parse_object ~take ~length
 
       (*String*)
       | 1::1::1::1::0::1::1::0::_ ->
-        take 6 >|= int_of_bytestring (module CCInt) ~of_int:CCFun.id
-          ~drop_bits_left:0 >>= fun length ->
+        take 6 >|= int_of_byte_string (module CCInt64) ~of_int:CCInt64.of_int
+          ~drop_bits_left:0 >|= CCInt64.to_int >>= fun length -> (*lossy*)
         parse_string ~take ~length
 
       (*Binary data*)
       | 1::1::1::1::0::1::1::1::_ ->
-        take 6 >|= int_of_bytestring (module CCInt) ~of_int:CCFun.id
-          ~drop_bits_left:0 >>= fun length ->
+        take 6 >|= int_of_byte_string (module CCInt64) ~of_int:CCInt64.of_int
+          ~drop_bits_left:0 >|= CCInt64.to_int >>= fun length -> (*lossy*)
         parse_binary_data ~take ~length
-
+    
       (*32bit signed integer*)
       | 1::1::1::1::1::0::0::0::_ ->
-        take 4 >|= int_of_bytestring (module CCInt32) ~of_int:CCInt32.of_int
+        take 4 >|= int_of_byte_string (module CCInt32) ~of_int:CCInt32.of_int
           ~drop_bits_left:0 >|= fun int ->
         `Float (Int32.float_of_bits int)
 
       (*32bit floating point number*)
       | 1::1::1::1::1::0::0::1::_ ->
-        take 4 >|= int_of_bytestring (module CCInt32) ~of_int:CCInt32.of_int
+        take 4 >|= int_of_byte_string (module CCInt32) ~of_int:CCInt32.of_int
           ~drop_bits_left:0 >|= fun int ->
         `Float (Int32.float_of_bits int)
 
       (*64bit floating point number*)
       | 1::1::1::1::1::0::1::0::_ ->
-        take 8 >|= int_of_bytestring (module CCInt64) ~of_int:CCInt64.of_int
+        take 8 >|= int_of_byte_string (module CCInt64) ~of_int:CCInt64.of_int
           ~drop_bits_left:0 >|= fun int ->
         `Float (Int64.float_of_bits int)
 
@@ -275,7 +274,7 @@ module Jsonr = struct
       take 1 >>= fun version' ->
       assert (version = version');
       take 1 >>= fun _reserved -> 
-      take 2 >|= int_of_bytestring (module CCInt) ~of_int:CCFun.id
+      take 2 >|= int_of_byte_string (module CCInt) ~of_int:CCFun.id
           ~drop_bits_left:4 >>= fun static_dictionary_size ->
       assert (static_dictionary_size <= 2048);
       parse_value ~take
@@ -291,6 +290,11 @@ end
       * take json + static dictionary as cli-params
       * init: construct a list of stream type clones containing json 
         * type: gen, seq, sequence ..
+  * test performance of a more low-level way of matching on bits
+    * (extra); cool lib; ppx-extension for pmatching on bits like a list 
+    * spec; 
+      * shift-right to match bit-block size
+      * pmatch on 0bxxx..?
 *)
 let run () =
   let open CCOpt.Infix in
